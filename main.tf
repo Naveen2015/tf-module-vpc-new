cb52b2baf30cb7255dcb96de0f100c5fdcea8615
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-01 13:28:32 -0500

    ngw

--- main.tf
+++ main.tf
@@ -30,15 +30,11 @@ resource "aws_eip" "eip" {
   count = length(lookup(lookup(var.subnets, "public",null), "cidr_block",0))
   tags = merge(var.tags, { Name = "${var.env}-eip-${count.index+1}"})
 }
-# resource "aws_nat_gateway" "example" {
-#   count = length(var.subnets["public"].cidr_block)
-#   allocation_id = aws_eip.eip[count.index].id
-#   subnet_id     = aws_subnet.
-#   tags = {
-#     Name = "gw NAT"
-#   }
-#
-#   # To ensure proper ordering, it is recommended to add an explicit dependency
-#   # on the Internet Gateway for the VPC.
-#   depends_on = [aws_internet_gateway.example]
-# }
+resource "aws_nat_gateway" "example" {
+  count = length(var.subnets["public"].cidr_block)
+  allocation_id = aws_eip.eip[count.index].id
+  subnet_id     = module.subnets["public"].subnet_ids[count.index]
+  tags = merge(var.tags, { Name = "${var.env}-ngw-${count.index+1}"})
+
+
+}

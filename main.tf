c32c1b5b941181697538d50a1bc970c299bf03af
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-10 13:41:00 -0500

    auto scalling group

--- main.tf
+++ main.tf
@@ -12,11 +12,11 @@ resource "aws_vpc" "main" {
 
 }
 
-resource "aws_vpc_peering_connection" "foo" {
-  peer_vpc_id   = var.default_vpc_id
-  vpc_id        = aws_vpc.main.id
-  auto_accept = true
-}
+# resource "aws_vpc_peering_connection" "foo" {
+#   peer_vpc_id   = var.default_vpc_id
+#   vpc_id        = aws_vpc.main.id
+#   auto_accept = true
+# }
 
 
 module "subnets" {

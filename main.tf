e0ed4f97c8573def0847aaedfa255b7d1b0d916f
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-10 17:12:16 -0500

    vpc peering

--- main.tf
+++ main.tf
@@ -12,12 +12,24 @@ resource "aws_vpc" "main" {
 
 }
 
-# resource "aws_vpc_peering_connection" "foo" {
-#   peer_vpc_id   = var.default_vpc_id
-#   vpc_id        = aws_vpc.main.id
-#   auto_accept = true
-# }
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept = true
+}
 
+resource "aws_route" "peering-connection-route" {
+  count = length(local.all_private_subnet_ids)
+  route_table_id = element(local.all_private_subnet_ids,count.index)
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  destination_cidr_block = var.default_vpc_cidr
+}
+resource "aws_route" "peering-connection-route-in-default-vpc" {
+
+  route_table_id = var.default_route_table_id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  destination_cidr_block = var.cidr_block
+}
 
 module "subnets" {
   source = "./subnets"
@@ -61,7 +73,4 @@ resource "aws_route" "ngw" {
   route_table_id = local.all_private_subnet_ids[count.index]
   nat_gateway_id = element(aws_nat_gateway.example.*.id,count.index)
   destination_cidr_block = "0.0.0.0/0"
-
-
-
 }
--- vars.tf
+++ vars.tf
@@ -2,4 +2,6 @@ variable "cidr_block" {}
 variable "tags" {}
 variable "subnets" {}
 variable "env" {}
-//variable "default_vpc_id" {}
+variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_route_table_id" {}
